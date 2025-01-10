846e861614480d86172f70e2948fe055fb6b7ee5
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-01-10 19:45:45 +0700

    Create  Maximum pair.cpp

---
+++ Maximum pair.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+using namespace std;
+const int mod = 1e9 + 7;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	sort(a.begin(), a.end());
+	cout << max(abs(a[0] + a[1]), abs(a[n - 1] + a[n - 2]));
+}
